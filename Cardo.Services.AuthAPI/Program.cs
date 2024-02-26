8fba387c38da7a6f426155eac814e22e86cafab0
TotalNodal <150134390+TotalNodal@users.noreply.github.com>
2024-02-26 22:21:37 +0100

    Implemented Auth Api

---
+++ Cardo.Services.AuthAPI/Program.cs
@@ -0,0 +1,56 @@
+using Cardo.Services.AuthAPI.Data;
+using Cardo.Services.AuthAPI.Models;
+using Cardo.Services.AuthAPI.Service;
+using Cardo.Services.AuthAPI.Service.IService;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    //retrieves connection string from appsettings.json and passes it to sql server
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("ApiSettings:JwtOptions"));
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+builder.Services.AddControllers();
+builder.Services.AddScoped<IJwTokenGenerator, JwtTokenGenerator>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+ApplyMigration();
+app.Run();
+
+// This will automatically apply any pending migrations
+void ApplyMigration()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        if (_db.Database.GetPendingMigrations().Count() > 0)
+        {
+            _db.Database.Migrate();
+        }
+    }
+}
---
+++ Cardo.Services.AuthAPI/Service/IService/IJwTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using Cardo.Services.AuthAPI.Models;
+
+namespace Cardo.Services.AuthAPI.Service.IService
+{
+    public interface IJwTokenGenerator
+    {
+        string GenerateToken(ApplicationUser applicationUser);
+    }
+}
